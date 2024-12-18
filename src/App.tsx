2d8dcbfee997744b4da64c5f5a2e5ea6bc3482de
bc-vsgd <benoit.charlesbc@yahoo.fr>
2024-12-18 13:00:46 +0100

    1st commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,17 @@
 import "./App.css";
-import { Box } from "@mui/material";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Header from "./components/Header";
+import HomePage from "./pages/HomePage";
 
 function App() {
-  return <Box>App</Box>;
+  return (
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
